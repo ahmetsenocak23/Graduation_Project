8a6342e058d0334b17e000e7b6c97ce858b58895
ahmetsenocak23 <167453206+ahmetsenocak23@users.noreply.github.com>
2025-09-26 18:15:20 +0300

    datasql.py

--- sql.py
+++ sql.py
@@ -1,14 +1,20 @@
+
+
 import sqlite3 
-con = sqlite3.connect("ask.db") # veri tabanına bağlantı, eğer veri tabanı yoksa dosya oluşturulacaktır.
-cur = con.cursor()
-from config import DATABASE
 
 class DB_Manager:
-    def create_table():
-        conn = sqlite3.connect("ask.db")
+    def __init__(self, db_name):
+        self.db_name = db_name 
+        self.create_table() 
+        
+    def create_table(self):
+        conn = sqlite3.connect(self.db_name) 
         cursor = conn.cursor()
+        
+        
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS all_questions (
+                kullanici_id INTEGER,
                 username TEXT,
                 question TEXT
             )
@@ -17,15 +23,24 @@ class DB_Manager:
         conn.commit()
         conn.close()
 
-
-    def get_all_questions():
-        conn = sqlite3.connect("ask.db")
+    def add_question(self, user_id, username, question_text):
+        conn = sqlite3.connect(self.db_name)
         cursor = conn.cursor()
+        cursor.execute("INSERT INTO all_questions (kullanici_id, username, question) VALUES (?, ?, ?)",(user_id, username, question_text))
+        conn.commit()
+        conn.close()
+        
+    def get_all_questions(self):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        
         cursor.execute("SELECT * FROM all_questions")
         questions = cursor.fetchall()
+        
         conn.close()
         return questions
 
-
 if __name__ == '__main__':
-    manager = DB_Manager(DATABASE)
+    DATABASE_NAME = "ask.db"
+    manager = DB_Manager(DATABASE_NAME)
+    
